599df8cb2128d2685759a2363ed40e24c7747545
yaoshuangxi <yaoshuangxi@kingsoft.com>
2016-07-25 15:02:26 +0800

    add project settings

--- Assets/Tako/TakoBuild.cs
+++ Assets/Tako/TakoBuild.cs
@@ -7,68 +7,57 @@ using System.Collections.Generic;
 
 class TakoBuild
 {
-	static string[] GetBuildScenes()
+	static string[] GetBuildScenes ()
 	{
-		List<string> names = new List<string>();
+		List<string> names = new List<string> ();
 
-		foreach(EditorBuildSettingsScene e in EditorBuildSettings.scenes)
-		{
-			if(e==null)
+		foreach (EditorBuildSettingsScene e in EditorBuildSettings.scenes) {
+			if (e == null)
 				continue;
 
-			if(e.enabled)
-				names.Add(e.path);
+			if (e.enabled)
+				names.Add (e.path);
 		}
-		return names.ToArray();
+		return names.ToArray ();
 	}
 
-	static string GetBuildPath()
+	static string GetBuildPath (BuildTarget target)
 	{
-		return "build/iOS";
+		if (target == BuildTarget.Android) {
+			return "build/android";
+		} else if (target == BuildTarget.iOS) {
+			return "build/ios";
+		}
+		return "build/unknow";
 	}
 
-	[UnityEditor.MenuItem("Tako/Test Command Line Build Step")]
+	static void Build (BuildTarget target)
+	{
+		Debug.Log ("Command line build version\n------------------------");
+
+		string[] scenes = GetBuildScenes ();
+		if (scenes == null || scenes.Length == 0) {
+			Debug.Log (string.Format ("Did not find any scene !!!"));
+			return;
+		}
+		for (int i = 0; i < scenes.Length; ++i) {
+			Debug.Log (string.Format ("Scene[{0}]: \"{1}\"", i, scenes [i]));
+		}
+		string path = GetBuildPath (target);
+		Debug.Log (string.Format ("Path: \"{0}\"", path));
+		Debug.Log ("Starting iOS Build!");
+		BuildPipeline.BuildPlayer (scenes, path, target, BuildOptions.None);
+	}
+
+	[UnityEditor.MenuItem ("Tako/Test Command Line Build Step iOS")]
 	static void BuildIOS ()
 	{
-		Debug.Log("Command line build\n------------------\n------------------");
-
-		string[] scenes = GetBuildScenes();
-		string path = GetBuildPath();
-		if(scenes == null || scenes.Length==0 || path == null)
-			return;
-
-		Debug.Log(string.Format("Path: \"{0}\"", path));
-		for(int i=0; i<scenes.Length; ++i)
-		{
-			Debug.Log(string.Format("Scene[{0}]: \"{1}\"", i, scenes[i]));
-		}
-
-		Debug.Log("Starting Build!");
-		BuildPipeline.BuildPlayer(scenes, path, BuildTarget.iOS, BuildOptions.None);
+		Build (BuildTarget.iOS);
 	}
 
-	static string GetBuildPathAndroid()
-	{
-		return "build/android";
-	}
-
-	[UnityEditor.MenuItem("Tako/Test Command Line Build Step Android")]
+	[UnityEditor.MenuItem ("Tako/Test Command Line Build Step Android")]
 	static void BuildAndroid ()
 	{
-		Debug.Log("Command line build android version\n------------------\n------------------");
-
-		string[] scenes = GetBuildScenes();
-		string path = GetBuildPathAndroid();
-		if(scenes == null || scenes.Length==0 || path == null)
-			return;
-
-		Debug.Log(string.Format("Path: \"{0}\"", path));
-		for(int i=0; i<scenes.Length; ++i)
-		{
-			Debug.Log(string.Format("Scene[{0}]: \"{1}\"", i, scenes[i]));
-		}
-
-		Debug.Log("Starting Android Build!");
-		BuildPipeline.BuildPlayer(scenes, path, BuildTarget.Android, BuildOptions.None);
+		Build (BuildTarget.Android);
 	}
 }
